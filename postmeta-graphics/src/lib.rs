64877654627b348b68be37fefe9c422bba51f769
Vadim Velicodnii <vadim@velikodniy.name>
2026-03-10 10:25:15 +1300

    chore: formatting

--- postmeta-graphics/src/lib.rs
+++ postmeta-graphics/src/lib.rs
@@ -10,14 +10,13 @@
     )
 )]
 
+pub mod bbox;
+pub mod bezier;
 pub mod error;
+pub mod intersection;
 pub mod math;
 pub mod path;
 pub mod pen;
+pub mod picture;
 pub mod transform;
 pub mod types;
-
-pub mod bbox;
-pub mod bezier;
-pub mod intersection;
-pub mod picture;
